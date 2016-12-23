377ab3243aa1e3222906d26986243fb0f65b3a7d
Darius Turner <dariusot731@gmail.com>
2016-12-22 22:54:30 -0500

    First commit
    committing project

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var miraclePillsImage: UIImageView!
+    @IBOutlet weak var miraclePillsLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var dividerLine: UIView!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var streetAddressLabel: UILabel!
+    @IBOutlet weak var streetTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var buyNowBtn: UIButton!
+    @IBOutlet weak var successImage: UIImageView!
+    
+    let states = ["Alaska", "Alabama", "Arkansas", "California", "Florida", "Georgia", "Maine", "Maryland", "Nevada", "North Carolina", "New York", "Oregon", "South Carolina", "Texas", "Washington"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +45,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryTextField.isHidden = true
+        zipCodeLabel.isHidden = true
+        zipCodeTextField.isHidden = true
+    }
+    
+    @IBAction func buyBtnPressed(_ sender: Any) {
+        miraclePillsImage.isHidden = true
+        miraclePillsLabel.isHidden = true
+        costLabel.isHidden = true
+        dividerLine.isHidden = true
+        fullNameLabel.isHidden = true
+        fullNameTextField.isHidden = true
+        streetAddressLabel.isHidden = true
+        streetTextField.isHidden = true
+        cityLabel.isHidden = true
+        cityTextField.isHidden = true
+        stateLabel.isHidden = true
+        statePicker.isHidden = true
+        statePickerBtn.isHidden = true
+        countryLabel.isHidden = true
+        countryTextField.isHidden = true
+        zipCodeLabel.isHidden = true
+        zipCodeTextField.isHidden = true
+        buyNowBtn.isHidden = true
+        successImage.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryTextField.isHidden = false
+        zipCodeLabel.isHidden = false
+        zipCodeTextField.isHidden = false
+    }
+    
 }
 
